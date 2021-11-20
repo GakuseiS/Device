c152383b2922904f5346b0118209136d2a151f95
Ilya Spiridonov <pikabusan@gmail.com>
2021-11-20 11:51:01 +0300

    added styled inputs in catalog

--- client/src/components/Catalog/Catalog.tsx
+++ client/src/components/Catalog/Catalog.tsx
@@ -16,23 +16,23 @@ export const Catalog: React.FC = () => {
                     </fieldset>
                     <fieldset className={'catalog__fieldset'}>
                         <legend className={'catalog__legend'}>Цвет</legend>
-                        <input id='black' type="checkbox" name='black' defaultChecked/>
-                        <label htmlFor="black">Черный</label>
-                        <input id='white' type="checkbox" name='white'/>
-                        <label htmlFor="white">Белый</label>
-                        <input id='blue' type="checkbox" name='blue' defaultChecked/>
-                        <label htmlFor="blue">Синий</label>
-                        <input id='red' type="checkbox" name='red' defaultChecked/>
-                        <label htmlFor="red">Красный</label>
-                        <input id='pink' type="checkbox" name='pink'/>
-                        <label htmlFor="pink">Розовый</label>
+                        <input className='catalog__checkbox' id='black' type="checkbox" name='black' defaultChecked/>
+                        <label className='catalog__label' htmlFor="black"><span className='catalog__custom-checkbox'></span> Черный</label>
+                        <input className='catalog__checkbox' id='white' type="checkbox" name='white'/>
+                        <label className='catalog__label' htmlFor="white"><span className='catalog__custom-checkbox'></span> Белый</label>
+                        <input className='catalog__checkbox' id='blue' type="checkbox" name='blue' defaultChecked/>
+                        <label className='catalog__label' htmlFor="blue"> <span className='catalog__custom-checkbox'></span> Синий</label>
+                        <input className='catalog__checkbox' id='red' type="checkbox" name='red' defaultChecked/>
+                        <label className='catalog__label' htmlFor="red"><span className='catalog__custom-checkbox'></span> Красный</label>
+                        <input className='catalog__checkbox' id='pink' type="checkbox" name='pink'/>
+                        <label className='catalog__label' htmlFor="pink"><span className='catalog__custom-checkbox'></span> Розовый</label>
                     </fieldset>
                     <fieldset className={'catalog__fieldset'}>
                         <legend className={'catalog__legend'}>Bluetooth</legend>
-                        <input id='bl-on' type="radio" name='bluetooth' defaultChecked/>
-                        <label htmlFor="bl-on">Есть</label>
-                        <input id='bl-off' type="radio" name='bluetooth'/>
-                        <label htmlFor="bl-off">Нет</label>
+                        <input className='catalog__radiobox' id='bl-on' type="radio" name='bluetooth' defaultChecked/>
+                        <label className='catalog__label' htmlFor="bl-on"><span className='catalog__custom-radiobox'></span> Есть</label>
+                        <input className='catalog__radiobox' id='bl-off' type="radio" name='bluetooth'/>
+                        <label className='catalog__label' htmlFor="bl-off"><span className='catalog__custom-radiobox'></span> Нет</label>
                     </fieldset>
                     <button className={'catalog__submit button'} type='submit'>Показать</button>
                 </form>
